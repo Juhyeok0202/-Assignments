3d7d5bc8424137133da0152c6f711f9af288e27d
Limwngur <ovg07047@naver.com>
2024-03-25 19:34:20 +0900

    Caesar: 경과 시간 측정 클래스 추가 및 100회 running 코드 추가

--- src/assignment2/caeser_cipher/Main.java
+++ src/assignment2/caeser_cipher/Main.java
@@ -1,20 +1,28 @@
 package assignment2.caeser_cipher;
 
-import assignment2.caeser_cipher.util.FileInDictionary;
-import assignment2.caeser_cipher.util.Generator;
-import assignment2.caeser_cipher.util.TextBuilder;
-import assignment2.caeser_cipher.util.Timer;
+import assignment2.caeser_cipher.util.*;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-    static final int privateKey = 3; //TODO 랜더으로
     static final String FILE_PATH = "C:/Project_files/computer_security/src/assignment2/words.txt";
-    private static final int ALPHABET_COUNT = 26; // 알파벳 글자 수
 
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("한 번 실행하고 모든 결과를 확인하려면 : 1");
+        System.out.println("100번 실행하고 각가의 실행 micro-sec와 평균 micro-sec값을 확인하려면 : 2");
 
+        int input = sc.nextInt();
+        if (input == 1) {
+            runOnce();
+        } else {
+            runOneHundred();
+        }
+    }
+
+    private static void runOnce() throws IOException {
         FileInDictionary fileInDictionary = FileInDictionary.initFileInDictionary(FILE_PATH); //Text(사전)파일에서 모든 단어를 추출하여 1.평문으로 만들 단어 리스트 2.사전 내 모든 단어리스트 를 초기화 한다.
 
         //private Key
@@ -35,6 +43,36 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void runOneHundred() throws IOException {
+        FileInDictionary fileInDictionary = FileInDictionary.initFileInDictionary(FILE_PATH); //Text(사전)파일에서 모든 단어를 추출하여 1.평문으로 만들 단어 리스트 2.사전 내 모든 단어리스트 를 초기화 한다.
+
+        int execCnt = 0;
+
+        while (execCnt++ < 100) {
+            //private Key
+            StringBuilder plainText = TextBuilder.buildPlainText(fileInDictionary.getWords()); // 사전의 단어를 랜덤으로 조합하여 평문을 만든다.
+            StringBuilder cipherText = TextBuilder.buildCipherText(plainText, Generator.generateRandomShiftNumber()); // 평문을 랜덤 쉬프트 값(1~25)을 이용하여 시저암호화 한다.
+
+
+            /**
+             * 암호 해독
+             */
+            try {
+                Cracker crack = new Cracker();
+                crack.crack(cipherText, fileInDictionary);
+
+                long time = Timer.calculateTotal();
+                System.out.println("["+execCnt+"번째]"+"경과시간(ms) : " + time);
+
+                Recoder.addTime(time);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("평균 경과 시간(ms) : "+Recoder.getAvgExecTime());
     }
 
     private static void showResult(StringBuilder plainText, String result) {
---
+++ src/assignment2/caeser_cipher/util/Recoder.java
@@ -0,0 +1,21 @@
+package assignment2.caeser_cipher.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recoder {
+    private static List<Long> execTimes = new ArrayList<>();
+
+    public static void addTime(long timeElapsed) {
+        execTimes.add(timeElapsed);
+    }
+
+    public static long getAvgExecTime() {
+        long sum = 0;
+        for (Long execTime : execTimes) {
+            sum += execTime;
+        }
+
+        return sum/ execTimes.size();
+    }
+}
